ad7675cd1c93ff3f9fb0fe5856649d9d9d57ae55
AFREED SHAIK <skafreed5@gmail.com>
2025-10-17 17:17:51 +0530

    created superuser

--- BACKEND/wegooo/wegoooapp/models.py
+++ BACKEND/wegooo/wegoooapp/models.py
@@ -7,6 +7,5 @@ class user(models.Model):
     password = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return self.username
