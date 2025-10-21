d579c6771b10f76a1ef747fd23bc9f7ea3fadff0
AFREED SHAIK <skafreed5@gmail.com>
2025-10-21 17:16:04 +0530

    updated

--- BACKEND/wegooo/api/urls.py
+++ BACKEND/wegooo/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('user/',views.UserListCreateAPIView.as_view(), name='user-list-create'),
     ]
--- BACKEND/wegooo/api/views.py
+++ BACKEND/wegooo/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from wegoooapp.models import User
+from .serializers import UserSerializer
 # Create your views here.
+class UserListCreateAPIView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
