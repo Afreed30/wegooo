6a9482b863294a9ae64722e638ec4e03f7e5583a
AFREED SHAIK <skafreed5@gmail.com>
2025-10-28 17:11:45 +0530

    add User registration serializer with password handling

--- BACKEND/wegooo/api/serializers.py
+++ BACKEND/wegooo/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import user, bus, route, schedule, seat, booking
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,3 +22,16 @@ class SeatSerializer(serializers.ModelSerializer):
     class Meta:
         model = seat
         fields = '__all__' 
+class UserregisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = user
+        fields = ['username', 'email', 'password']
+    def create(self, validated_data):
+        user_instance = user(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user_instance.set_password(validated_data['password'])
+        user_instance.save()
+        return user_instance
