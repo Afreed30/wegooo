1b5f1dd573911a79fea8e1bf4fd141e9eff6924c
AFREED SHAIK <skafreed5@gmail.com>
2025-10-28 20:17:07 +0530

    refactor: clean up admin.py by organizing imports and model registrations

--- BACKEND/wegooo/api/admin.py
+++ BACKEND/wegooo/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import bus, route, schedule
 # Register your models here.
+admin.site.register(bus)
+admin.site.register(route)
+admin.site.register(schedule)
